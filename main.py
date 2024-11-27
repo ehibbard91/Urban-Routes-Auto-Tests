ea9aa72d97b472b24d989696d1af64fc1fd34a66
Eric <eric.hibbard91@gmail.com>
2024-11-27 11:53:26 -0700

    Revised submission

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-import data
-import helpers
+import pytest
 
-from data import URBAN_ROUTES_URL
-# imports the URBAN_ROUTES_URL constant from data.py file, this will be updated when restarting server
-from helpers import is_url_reachable
-# imports server check from helpers.py file
+import data             #Imports constants from data.py
+import helpers          #Imports predefined functions from helpers.py
 
 class TestUrbanRoutes:
     @classmethod
     def setup_class(cls):
-        if is_url_reachable(URBAN_ROUTES_URL):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
             print("Connected to the Urban Routes server")
+    # Tests server response using function from helpers.py file. New servers require data.py url to be updated
         else:
             print("Cannot connect to Urban Routes. Check the server is on and still running")
-    # Tests server response using function from helpers.py file. New servers require data.py url to be updated
-
+            exit()
+        #Prints message and exits program when not connected to server
     def test_set_route(self):
         # Add in S8
         print('function created for set route')
@@ -46,7 +44,8 @@ class TestUrbanRoutes:
         pass
 
     def test_order_2_ice_creams(self):
-        for i in range(2):          #For loop that will iterate twice
+        num_ice_creams = 2     #Variable for number of ice creams. Could also be used in data.py
+        for i in range(num_ice_creams):   #For loop that will iterate depending on variable above
             # Add in S8
             print('function created for ice cream')
             pass
